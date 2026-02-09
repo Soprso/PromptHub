cc20773b53fac567dc5734b8cdee6478b4e349f3
soprso <ghoshsoumyadeep3@gmail.com>
2026-02-09 20:14:12 +0530

    Update custom option icon to match website style

--- src/pages/PromptBuilder.tsx
+++ src/pages/PromptBuilder.tsx
@@ -156,7 +156,7 @@ export default function PromptBuilder() {
                                                     {option}
                                                 </option>
                                             ))}
-                                            <option value="custom">✏️ Custom (type your own)</option>
+                                            <option value="custom">📝 Custom</option>
                                         </select>
 
                                         {variables[varName] === 'custom' && (
