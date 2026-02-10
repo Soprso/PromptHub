5003fb9a702f17160641931cc0f82db5477f7ac6
soprso <ghoshsoumyadeep3@gmail.com>
2026-02-11 02:02:26 +0530

    fix: Remove unused useEffect import to fix build

--- src/components/PromptForm.tsx
+++ src/components/PromptForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { communityApi } from '../lib/communityApi';
 import { containsProfanity, containsURL, checkSubmissionLimits, recordSubmission } from '../utils/validation';
 
